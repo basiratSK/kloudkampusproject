314ba8c29b40cd1833ef7ee08188d95e30d1fab8
basiratSK <121665670+basiratSK@users.noreply.github.com>
2023-05-31 15:52:15 +0530

    Update main.tf

--- notesapp/terraform/main.tf
+++ notesapp/terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
         required_providers {
         aws = {
-                source = "hashicorp/aws"
+                source="registry.terraform.io/hashicorp/aws"
                 version = "~> 4.16"
                 }
         }
@@ -10,6 +10,9 @@ required_version = ">=1.0.7"
 }
 provider "aws" {
 region = "ap-southeast-1"
+access_key = "AKIASS6VNYT4ORJNNGER"
+secret_key = "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"
+        
 }
 
 resource "aws_instance" "server" {
