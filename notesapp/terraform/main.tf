4eef615bf293a485e4eec7cc4e41e2e1b685dd0c
root <root@ip-172-31-31-243.ap-southeast-1.compute.internal>
2023-05-31 07:11:33 +0000

    Second Commit

---
+++ notesapp/terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+        required_providers {
+        aws = {
+                source = "hashicorp/aws"
+                version = "~> 4.16"
+                }
+        }
+
+required_version = ">=1.2.0"
+}
+provider "aws" {
+region = "ap-southeast-1"
+}
+
+resource "aws_instance" "server" {
+ count = 2
+ami = "ami-0df7a207adb9748c7"
+instance_type = "t2.medium"
+
+tags = {
+Name = "kloudkampus"
+}
+}
